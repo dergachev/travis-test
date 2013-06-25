3974b432fcc065a05b76de6aaa3d315889c43e84
Alex Dergachev <dergachev@gmail.com>
2013-06-25 15:52:41 -0300

    Added docker install

--- script.sh
+++ script.sh
@@ -7,4 +7,12 @@ echo "TIME:" $(date)
 sudo apt-get install apache2
 echo "TIME:" $(date)
 sudo apt-get install ffmpeg
+
+echo "Installing docker, TIME:" $(date)
+apt-get -y install python-software-properties
+add-apt-repository ppa:dotcloud/lxc-docker -y
+apt-get update
+apt-get -y install lxc-docker
+
+
 echo "Finished installing on TIME:" $(date)

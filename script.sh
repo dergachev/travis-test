ccacfbf2f63c97e4cc6259058f72dd4c88b95490
Alex Dergachev <dergachev@gmail.com>
2013-06-25 17:03:01 -0300

    add apparmor exception for lxc; via https://github.com/zpatten/lxc/blob/master/spec/support/install-lxc.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,18 @@ sudo apt-get install apache2
 echo "TIME:" $(date)
 sudo apt-get install ffmpeg
 
+
+## docker can't be installed due to this error: 
+### invoke-rc.d: initscript lxc-net, action "start" failed
+## the following workaround is via https://github.com/zpatten/lxc/blob/master/spec/support/install-lxc.sh
+cat <<EOF | sudo tee /usr/sbin/policy-rc.d
+#!/bin/sh
+exit 101
+EOF
+sudo chmod 755 /usr/sbin/policy-rc.d
+sudo apt-get -qq install lxc
+
+
 echo "Installing docker, TIME:" $(date)
 sudo apt-get -y install python-software-properties
 echo "TIME:" $(date)

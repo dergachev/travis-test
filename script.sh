4c8ba6683b7f31c1f4e3c4d4033a4b838f1a2585
Alex Dergachev <dergachev@gmail.com>
2013-06-25 15:56:37 -0300

    Added "sudo" prefix to docker installation commands.

--- script.sh
+++ script.sh
@@ -9,10 +9,13 @@ echo "TIME:" $(date)
 sudo apt-get install ffmpeg
 
 echo "Installing docker, TIME:" $(date)
-apt-get -y install python-software-properties
-add-apt-repository ppa:dotcloud/lxc-docker -y
-apt-get update
-apt-get -y install lxc-docker
+sudo apt-get -y install python-software-properties
+echo "TIME:" $(date)
+sudo add-apt-repository ppa:dotcloud/lxc-docker -y
+echo "TIME:" $(date)
+sudo apt-get update
+echo "TIME:" $(date)
+sudo apt-get -y install lxc-docker
 
 
 echo "Finished installing on TIME:" $(date)

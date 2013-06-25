d2fc3e8fc0dbd874cf17986348326e08d8be9852
Alex Dergachev <dergachev@gmail.com>
2013-06-25 15:48:03 -0300

    adding ffmpeg to build.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 # test script
 
-echo "Started on:" $(date)
+echo "Started on TIME:" $(date)
 
 sudo apt-get update
+echo "TIME:" $(date)
 sudo apt-get install apache2
-
-echo "Finished installing on:" $(date)
+echo "TIME:" $(date)
+sudo apt-get install ffmpeg
+echo "Finished installing on TIME:" $(date)
